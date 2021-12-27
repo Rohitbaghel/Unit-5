5c78c0467e32f96454681a25f985e3ec61b56628
Rohit Baghel <87422193+Rohitbaghel@users.noreply.github.com>
2021-12-27 23:13:20 +0530

    commit

---
+++ todo-list/src/App.jsx
@@ -0,0 +1,15 @@
+import {Grocery} from './component/Grocery';
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>GroceryList:Todo</h1>
+     <Grocery/>
+    </div>
+  );
+}
+
+export default App;
---
+++ todo-list/src/component/GroceryInput.jsx
@@ -0,0 +1,24 @@
+import {useState} from 'react'
+
+export const GroceryInput = ({GetItem}) =>  {
+
+    const [item,setItems] = useState("")
+    const GroceryItem = (e) => {
+        // console.log(e.target.value)
+        setItems(e.target.value)
+    }
+    const GroceryList = () => {
+    //   console.log(item)
+    GetItem(item)
+    }
+    return (
+       <>
+       <input type="text"  placeholder="Enter Your Grocery here..." onChange={GroceryItem}/>
+       <button onClick={GroceryList}>Add to the List</button>
+       
+       
+       </>
+    )
+}
+
+
