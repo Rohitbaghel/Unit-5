6d8a7f785537343668724db2f3dcf21667dededa
Rohit Baghel <87422193+Rohitbaghel@users.noreply.github.com>
2022-01-14 23:07:28 +0530

    commit

--- react-redux/src/store/store.js
+++ react-redux/src/store/store.js
@@ -1,9 +1,24 @@
-import {createStore,combineReducers} from "redux"
+import {createStore,combineReducers,applyMiddleware,compose} from "redux"
 import { reducer as TodoReducer} from "../Features/Todos/reducer";
 import { reducer as CounterReducer } from "../Features/Counter/reducer"
 const rootReducer = combineReducers( {
     counterState: CounterReducer,
     TodoState: TodoReducer,
 })
-export const store = createStore( rootReducer,window.__REDUX_DEVTOOLS_EXTENSION__() );
+const loader1=(store) => (next) => (action) => {
+    console.time('p1')
+    next(action);
+    console.timeEnd('p1')
+    
+}
+const loader2=(store) => (next) => (action) => {
+    console.time('p2')
+    next(action);
+    console.timeEnd('p2')
+    
+}
+export const store=createStore(rootReducer,
+    compose( window.__REDUX_DEVTOOLS_EXTENSION__(),  applyMiddleware(loader1, loader2)))
+  
+   
 console.log(store.getState())
